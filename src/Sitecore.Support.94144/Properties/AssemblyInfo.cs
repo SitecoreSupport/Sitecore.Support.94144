47e7b6ed299fdcd19a146b0373f9e10ea77530d5
Alexey Tupkalo <altu@sitecore.net>
2017-11-27 15:53:04 +0200

    Initial commit without a fix

---
+++ src/Sitecore.Support.94144/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94144")]
+[assembly: AssemblyProduct("Sitecore.Support.94144")]
+[assembly: ComVisible(false)]
